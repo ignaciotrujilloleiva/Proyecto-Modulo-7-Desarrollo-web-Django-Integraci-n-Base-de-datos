f3088099fbfc88cea4b05c93dc51cc31bdb0e13b
ignaciotrujilloleiva <ignacio.trujilloleiva@gmail.com>
2026-03-30 17:41:39 -0300

    Update CRUD usuario
    Creación de CRUD para usuario

---
+++ wallet/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    InicioView,
+    UsuarioListView,
+    UsuarioCreateView,
+    UsuarioUpdateView,
+    UsuarioDeleteView,
+)
+
+urlpatterns = [
+    path('', InicioView.as_view(), name='inicio'),
+    path('usuarios/', UsuarioListView.as_view(), name='usuario_list'),
+    path('usuarios/nuevo/', UsuarioCreateView.as_view(), name='usuario_create'),
+    path('usuarios/editar/<int:pk>/', UsuarioUpdateView.as_view(), name='usuario_update'),
+    path('usuarios/eliminar/<int:pk>/', UsuarioDeleteView.as_view(), name='usuario_delete'),
+]
--- wallet/views.py
+++ wallet/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
+
+from .models import Usuario
 
 # Create your views here.
+
+class InicioView(TemplateView):
+    template_name = 'wallet/inicio.html'
+
+
+class UsuarioListView(ListView):
+    model = Usuario
+    template_name = 'wallet/usuario_list.html'
+    context_object_name = 'usuarios'
+
+
+class UsuarioCreateView(CreateView):
+    model = Usuario
+    fields = ['nombre', 'correo', 'saldo']
+    template_name = 'wallet/usuario_form.html'
+    success_url = reverse_lazy('usuario_list')
+
+
+class UsuarioUpdateView(UpdateView):
+    model = Usuario
+    fields = ['nombre', 'correo', 'saldo']
+    template_name = 'wallet/usuario_form.html'
+    success_url = reverse_lazy('usuario_list')
+
+
+class UsuarioDeleteView(DeleteView):
+    model = Usuario
+    template_name = 'wallet/usuario_confirm_delete.html'
+    success_url = reverse_lazy('usuario_list')
