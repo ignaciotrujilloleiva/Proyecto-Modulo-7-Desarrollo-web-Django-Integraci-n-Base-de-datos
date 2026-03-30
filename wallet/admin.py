2b41d097522f76d38a8e746643483e37189d2dfc
ignaciotrujilloleiva <ignacio.trujilloleiva@gmail.com>
2026-03-30 16:40:10 -0300

    Update admin.py
    Configuración y carga de datos en panel admin

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+#Importacion de modelos 
+from .models import Usuario, Moneda, Transaccion
 
 # Register your models here.
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'correo', 'saldo', 'fecha_registro')
+    search_fields = ('nombre', 'correo')
+    list_filter = ('fecha_registro',)
+
+
+@admin.register(Moneda)
+class MonedaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'simbolo')
+    search_fields = ('nombre', 'simbolo')
+
+
+@admin.register(Transaccion)
+class TransaccionAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'moneda', 'monto', 'tipo_transaccion', 'fecha')
+    search_fields = ('usuario__nombre', 'moneda__nombre', 'tipo_transaccion')
+    list_filter = ('tipo_transaccion', 'moneda', 'fecha')
